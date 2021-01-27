c391a2df5fce0eea250995ec1244555ac9c153e1
himanshunete <himanshuneteh@gmail.com>
2021-01-27 07:07:18 +0530

    ADDED: functionality to find frequency of words and search

---
+++ HashTable/main/javapractiseuc3/MyLinkedList.java
@@ -0,0 +1,90 @@
+package javapractiseuc2;
+
+public class MyLinkedList<K> {
+	   public INode<K> head;
+	   public INode<K> tail;
+		
+		public MyLinkedList() {
+			this.head = null ;
+			this.tail = null ; 		
+		}
+		
+		public void add(INode<K> newNode) {
+			if (this.tail == null) {
+				this.tail = newNode ;
+			}
+			
+			if (this.head == null) {
+				this.head = newNode ;
+			}else {
+				INode<K> tempNode = this.head;
+				this.head = newNode ;
+				this.head.setNext(tempNode);
+			}
+			
+			
+		}
+		
+		public void append(INode<K> newNode) {
+			if (this.head == null) {
+				this.head = newNode ;
+			}
+			if (this.tail == null) {
+				this.tail = newNode ;
+			}else {
+				INode<K> tempNode = this.tail;
+				this.tail = newNode ;
+				tempNode.setNext(newNode);
+			}
+			
+		}
+		
+		public void insert(INode myNode, INode newNode) {
+			this.head.setNext(myNode);
+			myNode.setNext(newNode);
+			
+			
+		}
+		
+		public INode<K> search(K key) {
+			INode<K> tempNode = head ;
+			while (tempNode != null && tempNode.getNext() != null) {
+				if (tempNode.getKey().equals(key)) {
+					return tempNode ;
+				}
+				tempNode = tempNode.getNext() ;
+			}
+			return null ;
+		}
+		
+		public INode pop() {
+			INode  tempNode = this.head;
+			this.head = head.getNext();			
+			return tempNode;
+		}
+		
+		public INode popLast() {
+			INode tempNode = head;
+			while (!tempNode.getNext().equals(tail)){
+				tempNode = tempNode.getNext();
+			}
+			this.tail = tempNode;
+			tempNode = tail.getNext();
+			tempNode = null;
+			return tempNode ;
+		}
+		
+		public INode delete() {
+			INode tempNode1 = head ;
+			this.tail = tempNode1 ;
+			return tail; 
+		}	
+		
+		public void printMyNodes() {
+	       System.out.println("My Nodes: " + head);
+		}
+		
+		public String toString() {
+			return "MyLinkedListNodes{" + head + '}' ;
+		}
+	}
